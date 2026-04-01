3458327d19db396f95f94ee8714b0d557e539489
Javier Romo-Leon <j.arl@outlook.com>
2026-04-01 09:55:27 +0200

    Initial upload of class tutorials

---
+++ Tutorials/j25romol - example1.cpp
@@ -0,0 +1,22 @@
+
+//
+// Standard library includes
+//
+#include <iostream>
+
+
+
+using namespace std;  //On utilise un espace de nom ici
+
+
+int main()
+{
+   //
+   cout << "Hello " << endl;
+   cout << "C++ script complied by: " << endl;
+   cout << "Javier Alonzo ROMO LEON " << endl;
+   cout << "M.Sc. NE-PIA candidate: j25romol " << endl;
+   cout << "As of: 1 april 2026 " << endl;
+
+   return 0;
+};
