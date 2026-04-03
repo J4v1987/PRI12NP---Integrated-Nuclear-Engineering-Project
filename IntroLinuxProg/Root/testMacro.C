fa418eab89841e9398cbc2c1fb0575c8e6a4a29d
Javier Romo-Leon <j.arl@outlook.com>
2026-04-03 17:48:51 +0200

    revision comments

--- IntroLinuxProg/Root/testMacro.C
+++ IntroLinuxProg/Root/testMacro.C
@@ -1,35 +1,35 @@
 void testMacro()
 {
 
-  // on crée un hitogramme de 100 canaux pour des valeurs comprises entre -5 et 5
+  // on crÃ©e un histogramme de 100 canaux pour des valeurs comprises entre -5 et 5
   // la classe root s'appelle TH1F
-  // le "new" permet de faire une allocation dynamique de mémoire  
+  // le "new" permet de faire une allocation dynamique de mï¿½moire  
 
- TH1F *h1 = new TH1F("h1"," histogram ",100,-5,5);
- TH1F *h2 = new TH1F("h2"," histogram ",100,0,1000);
-  
+ //TH1F *h1 = new TH1F("h1"," histogram ",100,-5,5);    //j25romol: superseded in the context of the "Random number generator, ROOT. Laboratory: 'Initiation to Linux'"
+ //TH1F *h2 = new TH1F("h2"," histogram ",100,0,1000);  //j25romol: superseded in the context of the "Random number generator, ROOT. Laboratory: 'Initiation to Linux'"
+ TH1F *h3 = new TH1F("h3"," histogram ",100,-0.5,0.5);  //j25romol: assumed 100 bins are acceptable in the context of the "Random number generator, ROOT. Laboratory: 'Initiation to Linux'
 
- // on genere 1000000 nombres aléatoires x distribués suivant une loi uniforme entre -2 et 2 en utilisant le générateur de nombres pseudoaleatoires gRandom (classe TRandom de ROOT)
- // et on remplit l'histogramme h1
+ // on genere 1000000 nombres alÃ©atoires x distribuÃ©s suivant une loi uniforme entre -0.5 et 0.5 en utilisant le gÃ©nÃ©rateur de nombres pseudoaleatoires gRandom (classe TRandom de ROOT)
+ // et on remplit l'histogramme h3
 
   for (int i = 0; i < 1000000; i++) {
     float x = gRandom->Exp(100);
     //float x = gRandom->Uniform(-2,2);
-    h2->Fill(x);
+    //h2->Fill(x);  //j25romol: superseded in the context of the "Random number generator, ROOT. Laboratory: 'Initiation to Linux'"
+    h3->Fill(x);    //j25romol: filling of histogram 'h3' with 100 bins for random values in the range of [-0.5,0.5] for 1E6 random samples weighted to e^(-x/100), in the context of the "Random number generator, ROOT. Laboratory: 'Initiation to Linux'
   }
 
   // on dessine l'histogramme
 
-  h2->Draw();
+  //h2->Draw();
+  h3->Draw();
   
   // ADD THIS LINE:
-  gPad->SaveAs("alpha_results.pdf");
+  gPad->SaveAs("j25romol-alpha_results.pdf");
   //gPad->Print("alpha_results.pdf");
 
-  printf("Simulation complete. Histogram saved to 20260401-j25romol-alpha_results.pdf\n");
+  printf("Simulation complete. Histogram saved to j25romol-alpha_results.pdf\n");
   printf("C++ script compiled by:\n");
   printf("Javier Alonzo ROMO LEON.\n");
   printf("M.Sc. NE-PIA Candidate: j25romol.\n");
-  printf("As of: 1 april 2026.");
-
 }
