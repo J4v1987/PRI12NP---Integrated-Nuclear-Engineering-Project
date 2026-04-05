2857a5edcb12d8a58b3194639eb09a1ead485ce4
Javier Romo-Leon <j.arl@outlook.com>
2026-04-05 15:51:05 +0200

    Adding handle to AddEdep function header file B2EventAction and clarified visiblity scopes

--- TP GEANT4/B2a/B2a/include/B2EventAction.hh
+++ TP GEANT4/B2a/B2a/include/B2EventAction.hh
@@ -44,8 +44,12 @@ class B2EventAction : public G4UserEventAction
     virtual ~B2EventAction();
 
     virtual void  BeginOfEventAction(const G4Event* );
-    virtual void    EndOfEventAction(const G4Event* );
+    virtual void  EndOfEventAction(const G4Event* );
+
 /*j25romol: prefer the following revisions*/
+    void AddEdep(G4double edep);
+
+  private:
     G4double fEdep; //
 /*j25romol: end of advised revisions*/
     
