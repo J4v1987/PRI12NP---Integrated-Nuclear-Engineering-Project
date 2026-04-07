d950ce7dbb831b39c943bb27beb64d08a8054be0
Javier Romo-Leon <j.arl@outlook.com>
2026-04-07 11:21:05 +0200

    1MeV iteration

--- TP GEANT4/B2a/B2a-2/src/B2aDetectorConstruction.cc
+++ TP GEANT4/B2a/B2a-2/src/B2aDetectorConstruction.cc
@@ -139,7 +139,7 @@ G4VPhysicalVolume* B2aDetectorConstruction::DefineVolumes()
   G4double targetLength = 1.0*cm; // full length of Target (1 cm)
   G4double targetRadius  = 20*cm;   // Radius of Target
   
-  G4double trackerLength = 20*cm;
+  G4double trackerLength = 1*cm;//20*cm;
   G4double trackerRadius = 20*cm;
 
 
@@ -182,15 +182,16 @@ G4VPhysicalVolume* B2aDetectorConstruction::DefineVolumes()
     = new G4Tubs("target",0.,targetRadius,targetLength/2.,0.*deg,360.*deg);
   fLogicTarget
     = new G4LogicalVolume(targetS, fTargetMaterial,"Target",0,0,0);
-  //   new G4PVPlacement(0,               // no rotation
-  //                 positionTarget,  // at (x,y,z)
-  //                  fLogicTarget,    // its logical volume
-  //                "Target",        // its name
-  //                worldLV,         // its mother volume
-  //                false,           // no boolean operations
-  //                0,               // copy number
-  //                fCheckOverlaps); // checking overlaps 
-
+  /* 
+   new G4PVPlacement(0,               // no rotation
+                   positionTarget,  // at (x,y,z)
+                    fLogicTarget,    // its logical volume
+                  "Target",        // its name
+                  worldLV,         // its mother volume
+                  false,           // no boolean operations
+                  0,               // copy number
+                  fCheckOverlaps); // checking overlaps 
+  */
   G4cout << "Target is " << targetLength/cm << " cm of "
          << fTargetMaterial->GetName() << G4endl;
 
